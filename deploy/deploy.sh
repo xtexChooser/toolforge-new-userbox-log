c6e8f6b08d8d322ab9c4185019f53b636e711445
xtexChooser <xtexchooser@duck.com>
2023-01-06 21:46:56 +0800

    feat: track working dir on CD

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,9 @@
+pwd
+
 echo Load Toolforge jobs
 toolforge-jobs load ./deploy/jobs.yaml
 echo Compile logs:
 cat ./compile.out
 cat ./compile.err
+
 echo Deploy done
--- deploy/update.sh
+++ deploy/update.sh
@@ -1,7 +1,11 @@
+pwd
+
 echo Reset git...
 git reset --hard
 echo Pull git...
 git pull --force
+
 echo Run deploy script...
 bash ./deploy/deploy.sh
+
 echo Update done
