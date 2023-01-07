dc4a3d771bb64b8c5f652136aa93ed1bb74bb6fc
xtexChooser <xtexchooser@duck.com>
2023-01-07 11:35:40 +0800

    fix: start script can only be used for once

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,3 +1,4 @@
+toolforge-jobs delete update-manual | true
 toolforge-jobs run --image tf-node16 --command "cd src; yarn start" update-manual --wait
 toolforge-jobs show update-manual
 cat update-manual.out
