ed1eb69c54228ecae84890d67bfc11b1a3a41b6b
xtexChooser <xtexchooser@duck.com>
2023-01-07 10:57:12 +0800

    feat: show more details after manually run

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,1 +1,4 @@
-toolforge-jobs run --image tf-node16 --command "cd src; yarn start" update-manual
+toolforge-jobs run --image tf-node16 --command "cd src; yarn start" update-manual --wait
+toolforge-jobs show update-manual
+cat update-manual.out
+cat update-manual.err
