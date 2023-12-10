c24cc14e03a25c33356c95edb1b5c2f4a391790d
xtex <xtexchooser@duck.com>
2023-12-10 15:53:15 +0800

    build: update to node 18
    Signed-off-by: xtex <xtexchooser@duck.com>

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,5 +1,5 @@
 toolforge-jobs delete update-manual | true
-toolforge-jobs run --image tf-node16 --command "cd src; yarn start" update-manual --wait
+toolforge-jobs run --image node18 --command "cd src; yarn start" update-manual --wait
 toolforge-jobs show update-manual
 cat update-manual.out
 cat update-manual.err
